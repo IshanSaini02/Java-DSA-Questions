2f5176f3cd785f30a977acd2d022a1f4b8c7c11c
Ishan Saini <ishansaini382@gmail.com>
2025-11-18 22:44:27 +0530

    solve variable question

---
+++ src/VariableDataType/VariablePracticeQuestion4.java
@@ -0,0 +1,11 @@
+package VariableDataType;
+
+public class VariablePracticeQuestion4 {
+    public static void main(String[] args) {
+        int a = 1042, b = 7;
+
+        int quo = a/b , rem = a % b;
+
+        System.out.print(quo+ " " +rem);
+    }
+}
